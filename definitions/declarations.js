580b778fb23e29a9f35be4012adaaa59423a868d
Matheus Fuzati de carvalho <fuzatimatheus.cloud@gmail.com>
2026-03-09 21:24:51 +0000

    declarations.js

---
+++ definitions/declarations.js
@@ -0,0 +1,17 @@
+declare({
+  database: "toolkit-retail-media",
+  schema: "analytics_123456789",
+  name: "events_20260308",
+});
+
+declare({
+  database: "toolkit-retail-media",
+  schema: "google_ads_raw",
+  name: "campaign_performance_report",
+});
+
+declare({
+  database: "toolkit-retail-media",
+  schema: "salesforce_raw",
+  name: "Opportunity",
+});
